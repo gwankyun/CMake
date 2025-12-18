34d8ae530c23ee4375ceab3d51eaa8304bdf16d8
權少 <ljqican@163.com>
2025-12-18 20:44:48 +0800

    加入預設文件。

--- build_doc.ps1
+++ build_doc.ps1
@@ -1,6 +1,8 @@
-[string]$path = ".\build"
-if ($args.Length -gt 1)
-{
-    $path = $args[1]
-}
-cmake --build $path --target documentation --config Debug
+# [string]$path = ".\build"
+# if ($args.Length -gt 1)
+# {
+#     $path = $args[1]
+# }
+# cmake --build $path --target documentation --config Debug
+
+cmake --build --preset msvc
--- config_doc.ps1
+++ config_doc.ps1
@@ -1,6 +1,8 @@
-[string]$path = ".\build"
-if ($args.Length -gt 1)
-{
-    $path = $args[1]
-}
-cmake -S . -B $path -D SPHINX_HTML=ON
+# [string]$path = ".\build"
+# if ($args.Length -gt 1)
+# {
+#     $path = $args[1]
+# }
+# cmake -S . -B $path -D SPHINX_HTML=ON
+
+cmake --preset msvc
