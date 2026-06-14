01a770449b73cef321befdc1da20fbcdb7c8a875
權少 <ljqican@163.com>
2026-06-14 21:31:40 +0800

    文檔構造腳本支持regenerate-presets.py。

--- build_doc.ps1
+++ build_doc.ps1
@@ -5,4 +5,30 @@
 # }
 # cmake --build $path --target documentation --config Debug
 
+$current = $PWD
+$build = Join-Path $current "build"
+$fileOld = Join-Path $build "schema.yaml"
+$fileNew = Join-Path $current "Help/manual/presets/schema.yaml"
+
+$genPresets = $false
+
+if (-not (Test-Path $fileOld)) {
+    Copy-Item -Path $fileNew -Destination $build
+    $genPresets = $true
+}
+
+$itemOld = Get-Item $fileOld
+$itemNew = Get-Item $fileNew
+
+if ($itemNew.LastWriteTime -gt $itemOld.LastWriteTime) {
+    $genPresets = $true
+}
+
+if ($genPresets) {
+    Set-Location Utilities/Scripts
+    py regenerate-presets.py
+    Copy-Item -Path $fileNew -Destination $build
+    Set-Location $current
+}
+
 cmake --build --preset msvc
